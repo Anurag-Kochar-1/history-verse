4491d364f2bacaaccab6f5c6874a13a34a86c05a
Anurag <anuragmarketing101@gmail.com>
2023-02-25 20:45:18 +0530

    inital-commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className='text-9xl text-sky-400'> Hello History </h1>
+    </main>
+  )
+}
---
+++ context/AppContext.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppContextProvider = () => {
+    return (
+        <div>AppContext</div>
+    )
+}
+
+export default AppContextProvider
