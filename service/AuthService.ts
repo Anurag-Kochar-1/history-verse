ff70801e0f1ff9efdd3cdd375a3290b5883bf3bd
Anurag <anuragmarketing101@gmail.com>
2023-02-26 13:29:20 +0530

    SignInWithGoogleFunc-ADDED

--- service/AuthService.ts
+++ service/AuthService.ts
@@ -1,1 +1,46 @@
-export {}
+import { GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth"
+import { auth, db } from "../firebaseConfig"
+import { doc, getDoc, serverTimestamp, setDoc, updateDoc } from 'firebase/firestore'
+import { AppContext, IAppContextType } from "@/context/AppContext"
+import { useContext } from "react"
+
+
+export async function SignInWithGoogleFunction() {
+
+
+    const googleProvider = new GoogleAuthProvider()
+
+    try {
+        const res = await signInWithPopup(auth, googleProvider)
+        const userRef = doc(db, 'users', res?.user?.uid)
+        const userDoc = await getDoc(userRef)
+
+        if (userDoc.exists()) console.log("User Exits")
+        else if (!userDoc.exists()) {
+            await setDoc(doc(db, 'users', res?.user?.uid), {
+                userName: res?.user?.displayName,
+                displayPicture: res?.user?.photoURL,
+                userEmail: res?.user?.email,
+                userID: res?.user?.uid,
+
+                completedLessonsID: [],
+                userCoins: 100,
+                userProfileBanner: "",
+
+                appName: "History Verse"
+            })
+        }
+
+    } catch (error) {
+        alert(error)
+    }
+
+}
+
+export async function signOutUser() {
+    signOut(auth)
+}
+
+export function onAuthChanged(callback: (user: Object | null) => void) {
+    onAuthStateChanged(auth, callback)
+}
