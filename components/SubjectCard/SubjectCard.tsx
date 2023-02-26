f76a403d634a8ef7810935794e1e09249efee5c1
Anurag <anuragmarketing101@gmail.com>
2023-02-26 19:08:18 +0530

    tag-addded-on-card

--- components/SubjectCard/SubjectCard.tsx
+++ components/SubjectCard/SubjectCard.tsx
@@ -5,6 +5,7 @@ import { AppContext } from '@/context/AppContext'
 import { SignInWithGoogleFunction } from '@/service/AuthService'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
+import { FaFireAlt } from "react-icons/fa"
 
 const SubjectCard = ({ subject }: { subject: any }) => {
     const { userDetails } = useContext(AppContext)
@@ -49,7 +50,15 @@ const SubjectCard = ({ subject }: { subject: any }) => {
                 }
 
             }}
-            className={`${subject.className === 'Ancient Egypt' ? 'w-60 h-60 lg:w-96 lg:h-96' : "w-52 h-52 lg:w-80 lg:h-80"} relative rounded-md bg-brand m-2 flex justify-start items-end hover:scale-105 hover:cursor-pointer`}>
+            className={`${subject.className === 'Ancient Egypt' ? 'w-60 h-60 lg:w-96 lg:h-96 my-5' : "w-52 h-52 lg:w-80 lg:h-80"} relative rounded-md bg-brand m-2 flex justify-start items-end hover:scale-105 hover:cursor-pointer`}>
+
+            {subject?.className === 'Ancient Egypt' && (
+                <div className='z-20 absolute px-5 py-1 bg-red-500 -top-2 -left-2 text-white font-montserrat text-sm font-medium rounded-md flex justify-center items-center space-x-3'>
+                    <FaFireAlt className='text-white' size={"0.9rem"} />
+                    Most Popular
+                </div>
+            )}
+
             <Image
                 src={`${getBGImage(subject.className)}`}
                 alt="image"
