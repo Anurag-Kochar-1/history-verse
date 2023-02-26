696de5d208bdca79227e838a64cb2a44080b8d9b
Anurag <anuragmarketing101@gmail.com>
2023-02-26 19:56:18 +0530

    lintConfig-updated

--- next.config.js
+++ next.config.js
@@ -9,11 +9,7 @@ const nextConfig = {
 
       ]
     },
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
+  
   },
 }
 
