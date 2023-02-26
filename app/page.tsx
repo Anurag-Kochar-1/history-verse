952ec8aa15ec48c9e3efef919fda1e4d07a7a871
Anurag <anuragmarketing101@gmail.com>
2023-02-26 16:43:06 +0530

    getAllSubjects-Func-added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
 import NavBar from '@/components/Navbar/Navbar'
+import SubjectsContainer from '@/components/SubjectsContainer/SubjectsContainer'
+import { db } from '@/firebaseConfig'
+import { collection, getDocs } from 'firebase/firestore'
 import Image from 'next/image'
 
+async function getAllSubjects() {
+  const classesCollectionRef = collection(db, `/subjects/history/classes`)
+  const res = await getDocs(classesCollectionRef)
+  const data: any[] = res?.docs?.map(doc => doc.data())
+  console.log(data)
+  return data
+}
 
-export default function Home() {
+
+export default async function Home() {
+  const allSubjects = await getAllSubjects()
   return (
     <main className='w-full mt-[5rem] flex flex-col items-center justify-start'>
       <NavBar place='home' />
+      <SubjectsContainer allSubjects={allSubjects} />
     </main>
   )
 }
--- components/SubjectsContainer/SubjectsContainer.tsx
+++ components/SubjectsContainer/SubjectsContainer.tsx
@@ -1,9 +1,26 @@
+import Image from 'next/image'
 import React from 'react'
 
-const SubjectsContainer = () => {
-  return (
-    <div>SubjectsContainer</div>
-  )
+const SubjectsContainer = ( {allSubjects}: {allSubjects: any} ) => {
+    return (
+        <section className='relative w-full flex flex-col justify-center items-center py-28'>
+            {/* <Image
+                src={bg1}
+                alt="bg1"
+                width={1000}
+                height={1000}
+                className='-z-10 absolute w-full h-full object-cover bg-blend-multiply'
+            /> */}
+            <h2 className='text-7xl text-white font-montserrat font-bold text-center'> Top Classes </h2>
+            <p className='text-2xl text-white font-open_sans font-medium text-center my-2'> Learn about our Indian army in metaverse, Join class now! </p>
+
+            <div className='flex flex-wrap justify-center items-center my-5'>
+                {/* {allSubjects && allSubjects?.map((subject: any) => {
+                    return <ClassCard key={classObject?.classID} classObject={classObject} />
+                })} */}
+            </div>
+        </section>
+    )
 }
 
 export default SubjectsContainer
