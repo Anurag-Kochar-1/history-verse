6fff79309b59cf832e9ad48caf67fbe869f74a0e
Anurag <anuragmarketing101@gmail.com>
2023-02-26 18:39:16 +0530

    wallet-improved

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 interface IButtonProps {
-    children: string
+    children: string | React.ReactNode
     onClick?: () => void
     hidden?: boolean
     className?: string
@@ -12,7 +12,7 @@ const Button = ({ children, onClick, hidden, className }: IButtonProps) => {
         <button
             hidden={hidden}
             type='button'
-            title={children}
+            title={"btn"}
             onClick={onClick}
             className={`${className} min-w-[20px] h-10 px-5 font-open_sans text-white text-base font-semibold bg-brand rounded-md hover:bg-white hover:text-brand hover:border-2 hover:border-brand`}
         >
--- components/Wallet/Wallet.tsx
+++ components/Wallet/Wallet.tsx
@@ -1,6 +1,7 @@
 import { AppContext } from '@/context/AppContext'
 import { db } from '@/firebaseConfig'
 import { doc, getDoc } from 'firebase/firestore'
+import Image from 'next/image'
 import React, { useContext } from 'react'
 import Button from '../Button/Button'
 
@@ -16,11 +17,22 @@ const Wallet = () => {
 
 
     return (
-        <Button
-            className="hidden md:inline-block"
-        >
-            Wallet - 100 Gold Coins
-        </Button >
+        <div className='h-10 px-6 font-open_sans text-white flex justify-center items-center space-x-2 text-base font-semibold bg-dark rounded-md hover:bg-white hover:text-brand hover:border-2 hover:border-brand hover:cursor-pointer'>
+            <Image
+                src={`/goldCoin.png`}
+                alt={'goldCoin'}
+                width={100}
+                height={100}
+                className="w-6 h-6 "
+            />
+
+            <button
+                type='button'
+                title='wallet-button'
+            >
+                Wallet - 100 Gold Coins
+            </button>
+        </div>
 
     )
 }
